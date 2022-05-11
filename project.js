63fd5374c8e76b93bfef5702f265ecec6db66f96
An Nguyen <103458192+ElNopalito@users.noreply.github.com>
2022-05-10 23:29:50 -0400

    Add files via upload

---
+++ project.js
@@ -0,0 +1,66 @@
+const player = 'Player'
+const Computer = 'Computer'
+const Rock = 'Rock'
+const Scissor = 'Scissor'
+const Paper = 'Paper'
+const Boulder = 'Boulder'
+const generateVar = Math.floor(Math.random()*randomChoice.length)
+const randomChoice = [Rock, Paper, Scissor, Boulder]
+
+
+//! Set the array to spit out some random index number using Math.floor(Math.random()*4)
+//! Then set the random numer to a const random choice.
+//! Will have to set generateVar later 
+
+
+
+if(player == Computer){
+    console.log('Tie');
+}
+else if(player == [0]){
+    if(Computer == [1])
+    console.log('Player 2 Wins');
+}
+else(player == [0]){
+    if(c == [2]){
+    console.log('Player 1 Wins');
+}}
+ if(player == [0]){
+    if(Computer == [3]){
+    console.log('Player 2 Wins');
+}}
+else(Computer == [0]){
+    player == [1]{
+    console.log('Player 2 Wins');
+}}
+if(Computer === [0]){
+    player ===[2]
+    console.log('Player 1 Wins');
+}
+else(Computer === [0]){
+    if (player === [3]){
+    console.log('Player 1 wins');
+}}
+
+
+
+switch(generateVar){
+    case[0]:
+    console.log(Rock);
+        break;
+    case[1]:
+    console.log(Paper);
+        break;
+    case[2]:
+    console.log(Scissor);
+        break;
+    case[3]:
+    console.log(Boulder);
+        break;
+}
+
+
+
+Psuedo //! Will change to switch
+
+
